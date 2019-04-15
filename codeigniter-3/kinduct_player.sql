b67bff3d93bd7e8a0bf26885050c8e952904ec41
Vatsal Sood <vatsalsood@dal.ca>
2019-04-15 00:31:04 -0300

    sql file

--- codeigniter-3/kinduct_player.sql
+++ codeigniter-3/kinduct_player.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `kinduct` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `kinduct`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: kinduct
@@ -39,7 +41,7 @@ CREATE TABLE `player` (
 
 LOCK TABLES `player` WRITE;
 /*!40000 ALTER TABLE `player` DISABLE KEYS */;
-INSERT INTO `player` VALUES (68,'b',0,'b','b','b'),(69,'',0,'','',''),(70,'',0,'','',''),(71,'b',0,'b','b','b'),(72,'',0,'','',''),(73,'',0,'','',''),(74,'balakjd',29,'halifax','TasN','IasN'),(75,'bjd',29,'halifax','TasN','IasN'),(76,'vatsa',29,'','TasN','IasN'),(77,'bala',29,'halifax','TasN','IasN'),(78,'vatsa',29,'halifax','TasN','IasN'),(79,'bala',29,'halifax','TasN','IasN'),(80,'jaggu',29,'halifax','TasN','IasN'),(81,'xhellerx',99,'halifax','TasN','IasN'),(82,'xhellerx',99,'halifax','TasN','IasN'),(83,'yhellerx',299,'halifax','TasN','IasN'),(84,'xhellerx',99,'halifax','TasN','IasN'),(85,'pphellerx',299,'halifax','TasN','IasN'),(86,'xhellerx',99,'halifax','TasN','IasN'),(87,'pphellerx',299,'halifax','TasN','IasN'),(88,'xhellerx',99,'halifax','TasN','IasN'),(89,'xhellerx',99,'halifax','TasN','IasN'),(90,'xhellerx',99,'halifax','TasN','IasN'),(91,'woooo',299,'halifax','TasN','IasN'),(92,'xhellerx',99,'halifax','TasN','IasN'),(93,'xhellerx',99,'halifax','TasN','IasN'),(94,'xhellerx',99,'halifax','TasN','IasN'),(95,'poooo',299,'BC','TasN','IasN'),(96,'xhellerx',99,'halifax','TasN','IasN'),(97,'xhellerx',99,'halifax','TasN','IasN'),(98,'xhellerx',99,'halifax','TasN','IasN'),(99,'lpoooo',299,'Moncton','TasN','CA'),(100,'vatsa',29,'halifax','TasN','IasN'),(101,'vatsa',29,'halifax','TasN','IasN'),(102,'vatsa',29,'halifax','TasN','IasN'),(103,'xhellerx',99,'halifax','TasN','IasN'),(104,'lol',299,'Moncton','TasN','CA'),(105,'xhellerx',99,'halifax','TasN','IasN'),(106,'xhellerx',99,'halifax','TasN','IasN'),(107,'xhellerx',99,'halifax','TasN','IasN'),(108,'vatsa',29,'halifax','TasN','IasN'),(109,'vatsa',29,'halifax','TasN','IasN'),(110,'vatsa',29,'halifax','TasN','IasN'),(111,'vatsa',29,'halifax','TasN','IasN'),(112,'vatsa',29,'halifax','TasN','IasN'),(113,'vatsa',29,'halifax','TasN','IasN'),(114,'vatsa',29,'halifax','TasN','IasN'),(115,'vatsa',29,'halifax','TasN','IasN'),(116,'vatsa',29,'halifax','TasN','IasN'),(117,'vatsa',29,'halifax','TasN','IasN'),(118,'vatsa',29,'halifax','TasN','IasN'),(119,'vatsa',29,'halifax','TasN','IasN'),(120,'vatsa',29,'halifax','TasN','IasN'),(121,'vatsa',29,'halifax','TasN','IasN'),(122,'vatsa',29,'halifax','TasN','IasN'),(123,'vatsa',29,'halifax','TasN','IasN'),(124,'vatsa',29,'halifax','TasN','IasN'),(125,'bala',29,'halifax','TasN','IasN');
+INSERT INTO `player` VALUES (1,'vatsal',29,'halifax','ns','ca');
 /*!40000 ALTER TABLE `player` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-14 22:02:36
+-- Dump completed on 2019-04-15  0:27:02
